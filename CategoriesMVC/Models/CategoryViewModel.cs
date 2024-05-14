a71efb9f1b343b57de8bcb2a3f3d822f31fd8cf3
Giovani de Oliveira <giovani98@proton.me>
2024-05-14 11:40:42 -0300

    chore: Initial config

---
+++ CategoriesMVC/Models/CategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoriesMVC.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Display(Name = "Image")]
+        public string? ImageUrl { get; set; }
+    }
+}
--- CategoriesMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CategoriesMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
